6aedea9f3c67378ecbf446fee48104151cdd2364
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-01-05 15:58:30 +0100

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+// Напиши скрипт который,
+//     при наборе текста в инпуте input#name - i
+// nput(событие input), подставляет его текущее значение в
+// span#name - output.Если инпут пустой,
+//     в спане должна отображаться строка "Anonymous".
+
+const refs = {
+  inputEl: document.querySelector("#name-input"),
+  spanEl: document.querySelector("#name-output"),
+};
+
+function writeText(event) {
+  refs.spanEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    refs.spanEl.textContent = "Anonymous";
+  }
+}
+refs.inputEl.addEventListener("input", writeText);
+writeText();
